4789112b3735339d2a9d7ceb938714a79f08ad91
Sepehr <106178439+SepehrNaeij@users.noreply.github.com>
2022-09-08 16:26:50 +0430

    Add files via upload

---
+++ Technology/peydayesh.py
@@ -0,0 +1,11 @@
+def find(num1, num2, num3):
+    sum = num1 + num2 + num3
+    if sum == 6:
+        result = 4
+    elif sum == 7:
+        result = 3
+    elif sum == 8:
+        result = 2
+    elif sum == 9:
+        result = 1
+    return result                
